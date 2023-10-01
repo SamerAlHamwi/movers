099f1a41139e17c43c3b305531f8a22897f9baf7
afraa <af.h.1@hotmail.com>
2023-10-01 10:35:07 +0300

    test fixed login (expired)

--- src/components/router/RequireAuth.tsx
+++ src/components/router/RequireAuth.tsx
@@ -3,10 +3,27 @@ import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '@app/hooks/reduxHooks';
 import { WithChildrenProps } from '@app/types/generalTypes';
 
+// Function to calculate the expiration date based on expireInSeconds
+function calculateExpirationDate(expireInSeconds: any) {
+  const currentDate = new Date();
+  return new Date(currentDate.getTime() + expireInSeconds * 1000);
+}
+
 const RequireAuth: React.FC<WithChildrenProps> = ({ children }) => {
   const token = useAppSelector((state) => state.auth.token);
+  const expireInSeconds = 86400; // Token expiration time in seconds
+  const expirationDate = calculateExpirationDate(expireInSeconds);
 
-  return token !== 'bearerToken' ? <>{children}</> : <Navigate to="/auth/login" replace />;
+  const isTokenExpired = () => {
+    const currentDate = new Date();
+    return currentDate > expirationDate;
+  };
+
+  if (token === 'bearerToken' || isTokenExpired()) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
+  return <>{children}</>;
 };
 
 export default RequireAuth;
