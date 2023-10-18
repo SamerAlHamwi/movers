fe05fc3b121af8d93b8492627b434619940271f4
afraa <af.h.1@hotmail.com>
2023-10-18 17:22:24 +0300

    .

--- src/components/modal/AddPartner.tsx
+++ src/components/modal/AddPartner.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Space, Modal } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { BaseForm } from '@app/components/common/forms/BaseForm/BaseForm';
@@ -10,25 +10,45 @@ import { LableText } from '../GeneralStyles';
 import { useResponsive } from '@app/hooks/useResponsive';
 import { FONT_SIZE } from '@app/styles/themes/constants';
 import { Partner } from '@app/interfaces/interfaces';
+import { Select, Option } from '../common/selects/Select/Select';
+import { useQuery } from 'react-query';
+import { getCities, getCountries } from '@app/services/locations';
+
+const generateRandomCode = () => {
+  const min = 10000000;
+  const max = 99999999;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 export const AddPartner: React.FC<CreatePartnerModalProps> = ({ visible, onCancel, onCreatePartner, isLoading }) => {
   const [form] = BaseForm.useForm();
   const { t } = useTranslation();
   const { isDesktop, isTablet } = useResponsive();
 
-  const generateRandomCode = () => {
-    const min = 10000;
-    const max = 99999;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  const [countryId, setCountryId] = useState<string>('0');
+
+  const GetAllCountries = useQuery('GetAllCountries', getCountries);
+  const { data: citiesData, refetch: citiesRefetch } = useQuery('getCities', () => getCities(countryId), {
+    enabled: countryId !== '0',
+  });
+
+  useEffect(() => {
+    if (countryId !== '0') {
+      citiesRefetch();
+    }
+  }, [countryId]);
+
+  const ChangeCountryHandler = (e: any) => {
+    setCountryId(e);
+    form.setFieldValue('cityId', []);
   };
-  const [code, setCode] = useState(generateRandomCode());
 
   const onOk = () => {
     form.submit();
   };
 
   const onFinish = (PartnerInfo: Partner) => {
-    PartnerInfo = Object.assign({}, PartnerInfo, { isActive: true });
+    PartnerInfo = Object.assign({}, PartnerInfo);
     onCreatePartner(PartnerInfo);
   };
 
@@ -61,7 +81,6 @@ export const AddPartner: React.FC<CreatePartnerModalProps> = ({ visible, onCance
         <BaseForm.Item
           name="firstName"
           label={<LableText>{t('common.firstName')}</LableText>}
-          rules={[{ required: true, message: <p style={{ fontSize: FONT_SIZE.xs }}>{t('common.requiredField')}</p> }]}
           style={{ marginTop: '-.5rem' }}
         >
           <Input />
@@ -70,7 +89,14 @@ export const AddPartner: React.FC<CreatePartnerModalProps> = ({ visible, onCance
         <BaseForm.Item
           name="lastName"
           label={<LableText>{t('common.lastName')}</LableText>}
-          rules={[{ required: true, message: <p style={{ fontSize: FONT_SIZE.xs }}>{t('common.requiredField')}</p> }]}
+          style={{ marginTop: '-.5rem' }}
+        >
+          <Input />
+        </BaseForm.Item>
+
+        <BaseForm.Item
+          name="companyName"
+          label={<LableText>{t('brokers.companyName')}</LableText>}
           style={{ marginTop: '-.5rem' }}
         >
           <Input />
@@ -88,34 +114,37 @@ export const AddPartner: React.FC<CreatePartnerModalProps> = ({ visible, onCance
         <BaseForm.Item
           name="emailAddress"
           label={<LableText>{t('common.emailAddress')}</LableText>}
-          // rules={[{ required: true, message: <p style={{ fontSize: FONT_SIZE.xs }}>{t('common.requiredField')}</p> }]}
           style={{ marginTop: '-.5rem' }}
         >
           <Input />
         </BaseForm.Item>
 
         <BaseForm.Item
-          name="partnerCode"
-          initialValue={code}
-          label={<LableText>{t('partners.code')}</LableText>}
-          rules={[
-            {
-              required: true,
-              message: <p style={{ fontSize: FONT_SIZE.xs }}>{t('common.requiredField')}</p>,
-            },
-          ]}
-          style={{ marginTop: '-.5rem' }}
-        >
-          <Input />
-        </BaseForm.Item>
-
-        <BaseForm.Item
-          name="discountPercentage"
-          label={<LableText>{t('partners.discountPercentage')}</LableText>}
+          // name="countryId"
+          label={<LableText>{t('companies.country')}</LableText>}
           rules={[{ required: true, message: <p style={{ fontSize: FONT_SIZE.xs }}>{t('common.requiredField')}</p> }]}
-          style={{ marginTop: '-.5rem' }}
         >
-          <Input />
+          <Select onChange={ChangeCountryHandler}>
+            {GetAllCountries?.data?.data?.result?.items.map((country: any) => (
+              <Option key={country.id} value={country.id}>
+                {country?.name}
+              </Option>
+            ))}
+          </Select>
+        </BaseForm.Item>
+
+        <BaseForm.Item
+          name="cityId"
+          label={<LableText>{t('companies.city')}</LableText>}
+          rules={[{ required: true, message: <p style={{ fontSize: FONT_SIZE.xs }}>{t('common.requiredField')}</p> }]}
+        >
+          <Select mode="multiple">
+            {citiesData?.data?.result?.items.map((city: any) => (
+              <Select key={city.name} value={city.id}>
+                {city?.name}
+              </Select>
+            ))}
+          </Select>
         </BaseForm.Item>
       </BaseForm>
     </Modal>
