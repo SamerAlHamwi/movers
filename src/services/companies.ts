7711a5d710b4c529d0dfbc010f6c0658111eb752
afraa <af.h.1@hotmail.com>
2023-11-08 14:58:48 +0300

    get companies&branches witch was sent in details

--- src/services/companies.ts
+++ src/services/companies.ts
@@ -9,15 +9,27 @@ const getAllCompanies = async (page: number, pageSize: number, search: string) =
   );
 };
 
-const getSuitableCompanies = async (page: number, pageSize: number, search: string, requestId: string | undefined) => {
+const getSuitableCompanies = async (
+  type: string | undefined,
+  page: number,
+  pageSize: number,
+  search: string,
+  requestId: string | undefined,
+) => {
   const skip = (page - 1) * pageSize;
   return await httpApi.get(
-    `${apiPrefix.companies}/GetAll?statues=2&SkipCount=${skip}&MaxResultCount=${pageSize}&KeyWord=${search}&RequestId=${requestId}&IsForFilter=true`,
+    `${apiPrefix.companies}/GetAll?GetCompaniesWithRequest=${
+      type == '2' ? 'true' : 'false'
+    }&statues=2&SkipCount=${skip}&MaxResultCount=${pageSize}&KeyWord=${search}&RequestId=${requestId}&IsForFilter=true`,
   );
 };
 
 const getCompanyById = async (id: string | undefined) => {
   return await httpApi.get(`${apiPrefix.companies}/Get?Id=${id}`);
+};
+
+const GetReviewDetailsById = async (id: string | undefined) => {
+  return await httpApi.get(`${apiPrefix.companies}/GetReviewDetailsById?Id=${id}`);
 };
 
 const DeleteCompany = async (id: number) => {
@@ -40,6 +52,7 @@ export {
   getAllCompanies,
   getSuitableCompanies,
   getCompanyById,
+  GetReviewDetailsById,
   createCompany,
   updateCompany,
   DeleteCompany,
