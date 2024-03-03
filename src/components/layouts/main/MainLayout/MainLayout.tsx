b1f04524cffaaac4433f187737e8669bf63d7519
afraa <af.h.1@hotmail.com>
2024-03-03 14:34:53 +0300

    clean MainLayout file

--- src/components/layouts/main/MainLayout/MainLayout.tsx
+++ src/components/layouts/main/MainLayout/MainLayout.tsx
@@ -1,52 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Header } from '../../../header/Header';
 import MainSider from '../sider/MainSider/MainSider';
 import MainContent from '../MainContent/MainContent';
 import { MainHeader } from '../MainHeader/MainHeader';
 import * as S from './MainLayout.styles';
-import { Outlet, useLocation } from 'react-router-dom';
-// import { Statistics_Admin_PATH, Statistics_Manager_PATH } from '@app/components/router/AppRouter';
-import { useResponsive } from '@app/hooks/useResponsive';
 import { References } from '@app/components/common/References/References';
-import { useAppSelector } from '@app/hooks/reduxHooks';
-import { UserRole } from '@app/constants/userRole';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout: React.FC = () => {
-  const [isTwoColumnsLayout, setIsTwoColumnsLayout] = useState(true);
   const [siderCollapsed, setSiderCollapsed] = useState(true);
-  const { isDesktop } = useResponsive();
-  const location = useLocation();
-
-  const user = useAppSelector((state) => state.user.user);
-  const userRole = UserRole[user.userType];
 
   const toggleSider = () => setSiderCollapsed(!siderCollapsed);
-
-  useEffect(() => {
-    setIsTwoColumnsLayout(
-      (['/'].includes(location.pathname) ||
-        (location.pathname.startsWith('/my-games/rounds/') && location.pathname.endsWith('/game-course'))) &&
-        isDesktop,
-    );
-  }, [location.pathname, isDesktop]);
 
   return (
     <S.LayoutMaster>
       <MainSider isCollapsed={siderCollapsed} setCollapsed={setSiderCollapsed} />
       <S.LayoutMain $isStudent={false}>
-        <MainHeader isTwoColumnsLayout={isTwoColumnsLayout}>
+        <MainHeader isTwoColumnsLayout={false}>
           <Header
             toggleSider={toggleSider}
             isSiderOpened={!siderCollapsed}
-            isTwoColumnsLayout={isTwoColumnsLayout}
+            isTwoColumnsLayout={false}
             isStudent={false}
           />
         </MainHeader>
-        <MainContent id="main-content" $isTwoColumnsLayout={isTwoColumnsLayout}>
+        <MainContent id="main-content" $isTwoColumnsLayout={false}>
           <div>
             <Outlet />
           </div>
-          {!isTwoColumnsLayout && <References />}
+          <References />
         </MainContent>
       </S.LayoutMain>
     </S.LayoutMaster>
